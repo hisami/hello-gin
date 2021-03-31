767efc848c73bc5f7809406e4e7a1b89fe8a88b5
hisamitsu <ryohei.hisamitsu@ncdc.co.jp>
2021-03-31 21:44:03 +0900

    ミドルウェアを使ってみる

--- main.go
+++ main.go
@@ -8,10 +8,17 @@ import (
 
 func main() {
 	router := gin.Default()
+	ua := ""
 
+	// ミドルウェアを使用
+	router.Use(func(c *gin.Context) {
+		ua = c.GetHeader("User-Agent")
+		c.Next()
+	})
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello world!!",
+			"message":    "Hello world!!",
+			"User-Agent": ua,
 		})
 	})
 
